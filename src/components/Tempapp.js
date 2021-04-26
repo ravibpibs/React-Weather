3e2dadc0b26edc0577984a554fa06f68dbfb4eed
ravi ranjan jha <rjha4743@gmail.com>
2021-04-26 12:38:43 +0530

    first commit

---
+++ src/components/Tempapp.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import "./css/style.css";
+
+const Tempapp = () => {
+
+    const [city, setCity] = useState(null);
+    const [search, setSearch] = useState("delhi");
+
+    useEffect(() => {
+        const fetchApi = async () => {
+            const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=9816fd3c7827e823d841e5d3f8455cfc`
+            const response = await fetch(url)
+
+            const resJson = await response.json();
+            //console.log(resJson);
+            setCity(resJson.main);
+        }
+
+        fetchApi();
+    }, [search] )
+
+    return (
+        <>
+            <div className="box">
+                <div className="inputData">
+                    <input
+                        type="search"
+                        value={search}
+                        className="inputField"
+                        onChange={(event) => {
+                            setSearch(event.target.value)
+                        }} />
+                </div>
+                {!city ? (
+                    <p className="errorMsg">No Data Found</p>
+                ) : (
+                    <div>
+                        <div className="info">
+                            <h2 className="location">
+                                <i className="fas fa-street-view"></i>{search}
+                            </h2>
+                            <h1 className="temp">
+                                {city.temp}
+                            </h1>
+                            <h3 className="tempmin_max"> Min : {city.temp_min} °Cel | Max : {city.temp_max} °Cel </h3>
+                        </div>
+                        <div className="wave -one"></div>
+                        <div className="wave -two"></div>
+                        <div className="wave -three"></div>
+                    </div>
+                )
+
+                }
+            </div>
+
+        </>
+    )
+}
+
+export default Tempapp;
